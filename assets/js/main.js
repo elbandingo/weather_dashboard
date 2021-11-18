c60d1880d39c31bad795731a9fd1439c38439c5d
elbandingo <lewis.adam.r@gmail.com>
2021-11-18 17:33:22 -0330

    functionality almost theregit add -A

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,7 @@ var cityUvEl = document.querySelector("#city-uv");
 var buttonEl = document.querySelector("#search-btn");
 
 //create button handler when submit is clicked and set values
-buttonEl.addEventListener("click", function(e){
-    e.preventDefault();
+buttonEl.addEventListener("click", function(){
     var cityLocation = inputValueEl.value.trim();
     console.log("city name: " + cityLocation);
     if (cityLocation) {
@@ -27,15 +26,43 @@ var getWeatherData = function(city){
     
     //make the fetch request
     fetch(apiUrl).then(function(response){
-        //if the response is good, parse it to JSON, and send the data through
+        //if the response is good, parse it to JSON, and send the data through to 2 separate functions. 1 for main card, the other for the 5 day forecast
         if(response.ok) {
             response.json().then(function(data){
+                //set the initial variables for future display
+                var currLocation = data.name;
                 var currTemp = data.main.temp;
                 var currWind = data.wind.speed;
-                console.log(currTemp, currWind);
+                var currHum = data.main.humidity;
+                //capture the Lat and Lon values for another API call to get the UVI as its not included on the regular weather api
+                var uvLat = data.coord.lat;
+                var uvLon = data.coord.lon;
+                //var currUv = data.main.uvi;
+                console.log(currLocation, currTemp, currWind, currHum, uvLat, uvLon);
+                displayMainStats(currLocation,currTemp,currWind,currHum,uvLat,uvLon);
             })
         } else {
             alert("no city found!");
         }
+    });
+}
+
+var displayMainStats = function(name,temp,wind,humidity,uvLat,uvLon){
+    var uviUrl = 'http://api.openweathermap.org/data/2.5/onecall?lat='+ uvLat + '&lon='+ uvLon + '&dt=1586468027&appid=7bd6e4e882f11d63bf4270470fe9d78a';
+    fetch(uviUrl).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                var currUvi = data.current.uvi;
+                console.log("found lat and lon " + uvLat + uvLon);
+                cityUvEl.textContent = currUvi;
+            })
+        }
     })
+
+    cityNameEl.textContent = name + " (" + moment().format("MM/DD/YYYY") + ")";
+    cityTempEl.textContent = temp + " °C";
+    cityWindEl.textContent = wind + " Km/H";
+    cityHumidityEl.textContent = humidity + " %";
+    inputValueEl.value = "";
+
 }
