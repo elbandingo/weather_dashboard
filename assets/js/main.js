87340303a839bebfcc42383497917ce8ee24df74
elbandingo <lewis.adam.r@gmail.com>
2021-11-18 22:47:57 -0330

    wow..what progress. i feel like a coder now! this is almost shippable!

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,12 +34,13 @@ var getWeatherData = function(city){
                 var currTemp = data.main.temp;
                 var currWind = data.wind.speed;
                 var currHum = data.main.humidity;
+                var currIcon = data.weather[0].icon;
                 //capture the Lat and Lon values for another API call to get the UVI as its not included on the regular weather api
                 var uvLat = data.coord.lat;
                 var uvLon = data.coord.lon;
                 //var currUv = data.main.uvi;
-                console.log(currLocation, currTemp, currWind, currHum, uvLat, uvLon);
-                displayMainStats(currLocation,currTemp,currWind,currHum,uvLat,uvLon);
+                console.log(currLocation, currTemp, currWind, currHum, uvLat, uvLon, currIcon);
+                displayMainStats(currLocation,currTemp,currWind,currHum,uvLat,uvLon, currIcon);
             })
         } else {
             alert("no city found!");
@@ -47,14 +48,14 @@ var getWeatherData = function(city){
     });
 }
 
-var displayMainStats = function(name,temp,wind,humidity,uvLat,uvLon){
-    var uviUrl = 'http://api.openweathermap.org/data/2.5/onecall?lat='+ uvLat + '&lon='+ uvLon + '&dt=1586468027&appid=7bd6e4e882f11d63bf4270470fe9d78a';
+var displayMainStats = function(name,temp,wind,humidity,uvLat,uvLon,icon){
+    var uviUrl = 'http://api.openweathermap.org/data/2.5/onecall?lat='+ uvLat + '&lon='+ uvLon + '&units=metric&dt=1586468027&appid=7bd6e4e882f11d63bf4270470fe9d78a';
     fetch(uviUrl).then(function(response){
         if(response.ok){
             response.json().then(function(data){
                 var currUvi = data.current.uvi;
-                console.log("found lat and lon " + uvLat + uvLon);
                 
+                //logic for deciding what color uv index it will show based on numeric value
                 if(currUvi >= 7){
                     cityUvEl.classList = "badge bg-danger";
                     cityUvEl.textContent = currUvi;
@@ -69,14 +70,45 @@ var displayMainStats = function(name,temp,wind,humidity,uvLat,uvLon){
                     cityUvEl.classList = "badge bg-success";
                     cityUvEl.textContent = currUvi;
                 } 
+
+                displayFutureForecast(data);
+                
             })
         }
     })
-
+    console.log(icon);
     cityNameEl.textContent = name + " (" + moment().format("MM/DD/YYYY") + ")";
-    cityTempEl.textContent = temp + " °C";
+    cityTempEl.textContent = temp + " °C "
     cityWindEl.textContent = wind + " Km/H";
     cityHumidityEl.textContent = humidity + " %";
     inputValueEl.value = "";
+    var displayIconEl = document.createElement("img")
+    displayIconEl.src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+    cityNameEl.appendChild(displayIconEl);
+    
+
+}
+
+//display 5 day forecast
+var displayFutureForecast = function(data) {
+    //console log to make sure im getting the 7 days
+    var currDayInfo = data.daily;
+    //loop through each day, and add the elements to the card
+    for (var i = 0; i < 5; i++ ) {
+        var icon = currDayInfo[i].weather[0].icon;
+        var temp = currDayInfo[i].temp.day;
+        var wind = currDayInfo[i].wind_speed;
+        var humidity = currDayInfo[i].humidity;
+        console.log(icon, temp, wind, humidity);
+        var iconDisplay = document.querySelector("#day-" + [i]);
+        iconDisplay.src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        var tempDisplay = document.querySelector("#t-" + [i]);
+        tempDisplay.textContent = temp + " °C";
+        var windDisplay = document.querySelector("#w-" + [i]);
+        windDisplay.textContent = wind + " Km/H";
+        var HumDisplay = document.querySelector("#h-" + [i]);
+        HumDisplay.textContent = humidity;
+
+    }
 
 }
